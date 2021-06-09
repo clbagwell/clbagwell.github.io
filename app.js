1d7629871e19b7e749fcba9f47a2f6c4b5b61d67
bagwell <clbagwell@lbl.gov>
2021-06-08 17:52:30 -0700

    Removed alpha from renderer

--- app.js
+++ app.js
@@ -13,13 +13,14 @@ class App{
 		this.scene = new THREE.Scene();
         this.scene.background = new THREE.Color( 0xaaaaaa );
 
-		this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true } );
+		//this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true } );
+		this.renderer = new THREE.WebGLRenderer({ antialias: true } );
 		this.renderer.setPixelRatio( window.devicePixelRatio );
 		this.renderer.setSize( window.innerWidth, window.innerHeight );
 		container.appendChild( this.renderer.domElement );
 
 		this.renderer.setAnimationLoop(this.render.bind(this));
-		
+
         window.addEventListener('resize', this.resize.bind(this) );
 	}	
     
