c12204d1f8342ae4724f75cf5ebabed2c116d965
bagwell <clbagwell@lbl.gov>
2021-06-08 17:21:28 -0700

    Add basic WebGL ccomponents

--- app.js
+++ app.js
@@ -6,15 +6,29 @@ class App{
 		const container = document.createElement( 'div' );
 		document.body.appendChild( container );
     
+		// Add basic WebGL components: Camers, Scene, Renderer
+		this.camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 100 );
+		this.camera.position.set( 0, 0, 4 );
+        
+		this.scene = new THREE.Scene();
+        this.scene.background = new THREE.Color( 0xaaaaaa );
+
+		this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true } );
+		this.renderer.setPixelRatio( window.devicePixelRatio );
+		this.renderer.setSize( window.innerWidth, window.innerHeight );
+		container.appendChild( this.renderer.domElement );
+
         window.addEventListener('resize', this.resize.bind(this) );
 	}	
     
     resize(){
-        
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize( window.innerWidth, window.innerHeight ); 
     }
     
 	render( ) {   
-        
+        this.renderer.render( this.scene, this.camera );
     }
 }
 
