ee39a38e4e19dccafcc052af521c24c650d50e69
bagwell <clbagwell@lbl.gov>
2021-06-11 19:11:04 -0700

    Fixed indentation

--- app.js
+++ app.js
@@ -11,14 +11,14 @@ class App{
 		this.camera.position.set( 0, 0, 4 );
         
 		this.scene = new THREE.Scene();
-        this.scene.background = new THREE.Color( 0xaaaaaa );
+		this.scene.background = new THREE.Color( 0xaaaaaa );
 
 		const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 0.3);
 		this.scene.add(ambient);
         
-        const light = new THREE.DirectionalLight();
-        light.position.set( 0.2, 1, 1);
-        this.scene.add(light);
+		const light = new THREE.DirectionalLight();
+		light.position.set( 0.2, 1, 1);
+		this.scene.add(light);
 
 		//this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true } );
 		this.renderer = new THREE.WebGLRenderer({ antialias: true } );
@@ -27,39 +27,39 @@ class App{
 		container.appendChild( this.renderer.domElement );
 
 		const geometry = new THREE.BoxBufferGeometry();
-        const material = new THREE.MeshStandardMaterial( { color: 0xFF0000 });
+		const material = new THREE.MeshStandardMaterial( { color: 0xFF0000 });
 		const material2 = new THREE.MeshStandardMaterial( { color: 0x00FF00 });
 		const material3 = new THREE.MeshStandardMaterial( { color: 0x0000FF });
 
-        this.mesh = new THREE.Mesh( geometry, material );
+		this.mesh = new THREE.Mesh( geometry, material );
 		this.mesh2 = new THREE.Mesh( geometry, material2 );
 		this.mesh3 = new THREE.Mesh( geometry, material3 );
         
 		this.mesh2.position.x = -1.5;
 		this.mesh3.position.x = 1.5;
 
-        this.scene.add(this.mesh);
+		this.scene.add(this.mesh);
 		this.scene.add(this.mesh2);
 		this.scene.add(this.mesh3);
         
-        const controls = new OrbitControls( this.camera, this.renderer.domElement );
+		const controls = new OrbitControls( this.camera, this.renderer.domElement );
 
 		this.renderer.setAnimationLoop(this.render.bind(this));
 
-        window.addEventListener('resize', this.resize.bind(this) );
+		window.addEventListener('resize', this.resize.bind(this) );
 	}	
     
     resize(){
-        this.camera.aspect = window.innerWidth / window.innerHeight;
-        this.camera.updateProjectionMatrix();
-        this.renderer.setSize( window.innerWidth, window.innerHeight ); 
+		this.camera.aspect = window.innerWidth / window.innerHeight;
+		this.camera.updateProjectionMatrix();
+		this.renderer.setSize( window.innerWidth, window.innerHeight ); 
     }
     
 	render( ) {   
 		this.mesh.rotateY( 0.01 );
-		this.mesh2.rotateY( 0.01 );
-		this.mesh3.rotateY( 0.01 );
-        this.renderer.render( this.scene, this.camera );
+		this.mesh2.rotateY( -0.01 );
+		this.mesh3.rotateY( -0.01 );
+		this.renderer.render( this.scene, this.camera );
     }
 }
 
