a2ba468e91d0bfc7ddce5c195c0ed69beb3c2d05
bagwell <clbagwell@lbl.gov>
2021-06-25 11:02:11 -0700

    Comment out createButton call

--- VR_Ball_Room/app.js
+++ VR_Ball_Room/app.js
@@ -82,7 +82,7 @@ class App{
     
     setupVR(){
         this.renderer.xr.enabled = true;
-        document.body.appendChild( VRButton.createButton( this.renderer ) );
+        // document.body.appendChild( VRButton.createButton( this.renderer ) );
 
         const button = new VRButton( this.renderer );
         
